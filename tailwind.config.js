9b3d809812e5c58f69f2dcbb45ebc596aaa97719
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-02-03 10:47:48 +0100

    adding config + tw + npm + plugin

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["index.html"];
+export const theme = {
+  extend: {
+    colors: {
+      balticSea: "#24232C",
+    },
+  },
+};
+export const plugins = [];
