b6148fc898bc6bc8cf6f9af61e74099aac5bd0b2
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-02-03 11:55:03 +0100

    adding unit test

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-function generateString(length, hasLowercase, hasUppercase) {
+/**
+ * Fonction qui génère mdp
+ * @param {Number} length - Taille du mdp
+ * @param {boolean} hasLowerCase - Minuscule dans le mdp
+ * @param {boolean} hasUpperCase - Majuscule dans le mdp
+ * @returns MDP
+ */
+export const generateString = (length, hasLowerCase, hasUpperCase) => {
   const lowercase = "abcdefghijklmnopqrstuvwxyz";
   const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let result = " ";
-  let letters = " ";
+  let result = "";
+  let letters = "";
+
+  if (hasLowerCase) letters += lowercase;
+  if (hasUpperCase) letters += uppercase;
+
   const charactersLength = letters.length;
-
-  if (hasLowercase) {
-    letters += lowercase;
-  }
-
-  if (hasUppercase) {
-    letters += uppercase;
-  }
 
   for (let i = 0; i < length; i++) {
     result += letters.charAt(Math.floor(Math.random() * charactersLength));
   }
 
   return result;
-}
+};
 
-console.log(generateString(5, true));
+console.log(generateString(10, true, true));
