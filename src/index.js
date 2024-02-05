d772bbfb1dd103a9e72f353fd8f38ce7e5b8b501
JodieAddis <jodie.addis.92@gmail.com>
2024-02-05 11:59:12 +0100

    Complete generate function with news characters. Add unit test for news characters. Select checkbox and addition of event listener for each input.

--- src/index.js
+++ src/index.js
@@ -3,24 +3,55 @@
  * @param {Number} length - Taille du mdp
  * @param {boolean} hasLowerCase - Minuscule dans le mdp
  * @param {boolean} hasUpperCase - Majuscule dans le mdp
+ * @param {boolean} hasNumber - Chiffres dans le mdp
+ * @param {boolean} hasSymbols - Symbols dans le mdp
  * @returns MDP
  */
-export const generateString = (length, hasLowerCase, hasUpperCase) => {
+export const generateString = (
+  length,
+  hasLowerCase,
+  hasUpperCase,
+  hasNumbers,
+  hasSymbols,
+) => {
   const lowercase = "abcdefghijklmnopqrstuvwxyz";
   const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*()";
   let result = "";
-  let letters = "";
+  let characters = "";
 
-  if (hasLowerCase) letters += lowercase;
-  if (hasUpperCase) letters += uppercase;
+  if (hasLowerCase) characters += lowercase;
+  if (hasUpperCase) characters += uppercase;
+  if (hasNumbers) characters += numbers;
+  if (hasSymbols) characters += symbols;
 
-  const charactersLength = letters.length;
+  const charactersLength = characters.length;
 
   for (let i = 0; i < length; i++) {
-    result += letters.charAt(Math.floor(Math.random() * charactersLength));
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
 
   return result;
 };
 
-console.log(generateString(10, true, true));
+//console.log(generateString(10, true, true, true, true));
+
+const lowercase = document.getElementById("password-lowercase");
+const uppercase = document.getElementById("password-uppercase");
+const numbers = document.getElementById("password-numbers");
+const symbols = document.getElementById("password-symbols");
+
+const checkboxIsChecked = () => {
+  if (numbers.checked == true) {
+    //alert("checkbox checked");
+    console.log(generateString(10, false, false, true, false));
+  } else if (lowercase.checked == true) {
+    console.log(generateString(10, true, false, false, false));
+  }
+};
+
+lowercase.addEventListener("change", checkboxIsChecked);
+uppercase.addEventListener("change", checkboxIsChecked);
+numbers.addEventListener("change", checkboxIsChecked);
+symbols.addEventListener("change", checkboxIsChecked);
