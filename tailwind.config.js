c5e6821a9aea00ed8a02313a965682240ce20b71
JodieAddis <jodie.addis.92@gmail.com>
2024-02-06 17:06:13 +0100

    Design of the generator with checkboxes, text fields, ranges, and buttons + organization of the elements.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ export const theme = {
   extend: {
     colors: {
       balticSea: "#24232C",
+      darkBalticSea: "#18171F",
+      topaz: "#817D92",
+      mischka: "#E6E5EA",
+      mintGreen: "#A4FFAF",
+      red: "#F64A4A",
+      coral: "#FB7C58",
+      yellow: "#F8CD65",
     },
   },
 };
