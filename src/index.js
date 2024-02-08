41f9ef9a9ec7605d6774607215df6697ef4f8d0f
JodieAddis <jodie.addis.92@gmail.com>
2024-02-08 11:47:57 +0100

    Generate password with a choosen length.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ const handleSubmit = () => {
   const lowercaseChecked = document.getElementById("password-lowercase");
   const uppercaseChecked = document.getElementById("password-uppercase");
   const numbersChecked = document.getElementById("password-numbers");
-  console.log(numbersChecked);
   const symbolsChecked = document.getElementById("password-symbols");
   const form = document.querySelector(".form");
   const displayPassword = document.querySelector("#display-password");
+  const lengthDisplay = document.querySelector("#lengthDisplay");
+  const passwordLength = document.querySelector("#password-length");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const password = generatePassword(
-      10,
+      passwordLength.value,
       lowercaseChecked.checked,
       uppercaseChecked.checked,
       numbersChecked.checked,
@@ -22,6 +23,7 @@ const handleSubmit = () => {
     displayPassword.innerHTML = password;
     clipboardText(password);
   });
+  lengthDisplay.innerHTML = slider.value;
 };
 
 handleSubmit();
