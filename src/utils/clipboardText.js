2d6deccc22ed818472fa5aad83169f26bc5da264
JodieAddis <jodie.addis.92@gmail.com>
2024-02-08 10:45:06 +0100

    hidden text + color icon change with setTimeOut.

--- src/utils/clipboardText.js
+++ src/utils/clipboardText.js
@@ -5,9 +5,15 @@
 export const clipboardText = (password) => {
   const copiedText = document.querySelector("#copiedText");
   const copyBtn = document.querySelector("#copyBtn");
+  const copyIcon = document.querySelector("#copyIcon");
   //const test = "test de copie";
   copyBtn.addEventListener("click", () => {
     navigator.clipboard.writeText(password);
     copiedText.innerHTML = "Copied";
   });
+  setTimeout(() => {
+    copiedText.innerHTML = "";
+    copyIcon.setAttribute("class", "text-mintGreen");
+    //console.log("copied disparait après 5sec");
+  }, 5000);
 };
