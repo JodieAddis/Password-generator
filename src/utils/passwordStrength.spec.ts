df88fba8e5dcc53b72a0cf2fd6a9db94be8967c3
JodieAddis <jodie.addis.92@gmail.com>
2024-02-14 11:52:35 +0100

    Refactoring of passwordStrength function code + attempts Unit Test Suite for strength function.

--- src/utils/passwordStrength.spec.ts
+++ src/utils/passwordStrength.spec.ts
@@ -1,2 +1,25 @@
 import { describe, it, expect } from "vitest";
-import { passwordStrength } from "./passwordStrength.js";
+import {
+  getInputElements,
+  countCheckedInputs,
+  strengthLevel,
+} from "./passwordStrength";
+
+describe("countCheckedInputs Unit Test Suite", () => {
+  it("should return something the number of checked checkbox", () => {
+    //Simulation de checkbox cochées ou non:
+    const inputTest1 = { checked: true } as HTMLInputElement;
+
+    const inputTest2 = { checked: false } as HTMLInputElement;
+
+    const inputTest3 = { checked: true } as HTMLInputElement;
+
+    const result = countCheckedInputs([inputTest1, inputTest2, inputTest3]);
+
+    expect(result).toBe(2);
+  });
+});
+
+describe("strengthLevel Unit Test Suite", () => {
+  it("should return something ", () => {});
+});
--- src/utils/passwordStrength.ts
+++ src/utils/passwordStrength.ts
@@ -1,3 +1,4 @@
+/*
 export const passwordStrength = (): void => {
   const inputCheckbox = document.querySelectorAll(
     ".input_checkbox",
@@ -23,6 +24,65 @@ export const passwordStrength = (): void => {
     "medium",
   );
   strengthDisplay.classList.remove("strength");
+  switch (count) {
+    case 1:
+      strength.innerHTML = "too weak !";
+      strengthContainer.classList.add("too_weak");
+      break;
+    case 2:
+      strength.innerHTML = "weak";
+      strengthContainer.classList.add("weak");
+      break;
+    case 3:
+      strength.innerHTML = "medium";
+      strengthContainer.classList.add("medium");
+      break;
+    case 4:
+      strength.innerHTML = "strong";
+      strengthDisplay.classList.add("strong");
+      break;
+  }
+};
+*/
+
+export const getInputElements = (): {
+  inputCheckbox: NodeListOf<HTMLInputElement>;
+  strength: HTMLInputElement;
+  strengthDisplay: HTMLElement;
+  strengthContainer: HTMLElement;
+} => {
+  const inputCheckbox = document.querySelectorAll(
+    ".input_checkbox",
+  ) as NodeListOf<HTMLInputElement>;
+  const strength = document.querySelector("#strengthLevel") as HTMLInputElement;
+  const strengthDisplay = document.querySelector(".strength") as HTMLElement;
+  const strengthContainer = document.querySelector(
+    ".strength_container",
+  ) as HTMLElement;
+
+  return { inputCheckbox, strength, strengthDisplay, strengthContainer };
+};
+
+export const countCheckedInputs = (
+  inputCheckbox: HTMLInputElement[],
+): number => {
+  let count = 0;
+
+  for (let i = 0; i < inputCheckbox.length; i++) {
+    if (inputCheckbox[i].checked == true) {
+      count++;
+      console.log(count);
+    }
+  }
+  return count;
+};
+
+export const strengthLevel = (
+  count: number,
+  strength: HTMLSpanElement,
+  strengthContainer: HTMLElement,
+  strengthDisplay: HTMLElement,
+): void => {
   switch (count) {
     case 1:
       strength.innerHTML = "too weak !";
